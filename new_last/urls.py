f951b60c17fa3b336ade00b92e7b3f4f187faffa
eternty <eternty1993@gmail.com>
2015-11-14 18:09:14 +0300

    after migrations have been applied 2

--- new_last/urls.py
+++ new_last/urls.py
@@ -23,6 +23,6 @@ from django.contrib import admin
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
-    url(r'^$', views.index)
-
+    url(r'^$', views.index),
+    url(r'^views.questions')
 ]
--- project/views.py
+++ project/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
-
+from project.models import Question, QuestionOrder, Answer, SystemObject,AttributeValue, Attribute, \
+    AttributeAnswer,RulesAttribute
 # Create your views here.
 def index(request):
     session = request.session
@@ -11,5 +12,32 @@ def index(request):
         session['b'] = 'b'
 
     return render(request, 'index.html', args)
+
+def init_session(request):
+    session = {
+        "asked_questions": [],
+        "chosed_answers": [],
+        "applied_rules": [],
+    }
+    request.session = session
+
+def update_sessions(request,inc_questions = False,finished=False, write_results=False, results = "" ):
+    if inc_questions:
+        request.session['history']['questions'] += 1
+    if write_results:
+        request.session['history']['results'] = results
+    if finished:
+def questions(request):
+    session = request.session
+    answers = []
+    if session.
+    questions = Question.objects.get(all()),
+    context = {
+
+
+    }
+    if request.method == 'GET':
+
+    elif request.method == 'POST':
 
 
